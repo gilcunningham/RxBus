7b9c03b78f5ac6fdda05e08371dd6475a431b9b0
Gil Cunningham <gilcunningham@gmail.com>
2017-07-19 13:28:11 -0400

    Modified path

--- demo/src/main/java/demo/rxbus/event/ActivityToActivityEvent.java
+++ demo/src/main/java/demo/rxbus/event/ActivityToActivityEvent.java
@@ -4,9 +4,9 @@ package demo.rxbus.event;
  * Created by gil.cunningham on 6/7/2017.
  */
 
-public class ActivityEvent extends Event {
+public class ActivityToActivityEvent extends Event {
 
-    public ActivityEvent(String message) {
+    public ActivityActivityEvent(String message) {
         super(message);
     }
 }
